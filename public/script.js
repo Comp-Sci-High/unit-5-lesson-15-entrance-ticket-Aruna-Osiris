a5e28e5894b4292fb00e669083d2536363953d0a
Aruna-Osiris <rihanna.purandah25@compscihigh.org>
2025-04-08 13:59:41 +0000

    finished

--- public/script.js
+++ public/script.js
@@ -1,7 +1,33 @@
 // Create a function that tests the post route handler [2 pts]
 
+const base = 'https://reimagined-acorn-6949w45x9qqw2575w-3000.app.github.dev'
+
+async function newPotion(label, effect, ingredients, color, isExplosive) {
+    try {
+        // fill in the path
+        const response = await fetch(base + "/add" , {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                label: label,
+                effect: effect,
+                ingredients: ingredients,
+                color: color,
+                isExplosive: isExplosive
+            })
+
+        });
+        const json = await response.json();
+        console.log(json);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
 
 
 // Call the function [1 pt]
 // Show us the successful POST fetch [1 pt]
-
+newPotion("Knowledge Perfection 1", "gain knowledge beyond the universe", "nerves, blood of the user, mint, a piece of heaven", "blue", false)
